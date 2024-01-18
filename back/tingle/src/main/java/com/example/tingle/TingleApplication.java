803a1755e7e933ffb8ac72266ba6d6d497cb219f
cksghkd123 <cksghkd1234@naver.com>
2024-01-18 10:05:42 +0900

    initial commit

---
+++ back/tingle/src/main/java/com/example/tingle/TingleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TingleApplication.class, args);
+    }
+
+}
