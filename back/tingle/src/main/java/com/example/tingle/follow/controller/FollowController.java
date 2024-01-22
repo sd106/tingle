84b52a8c351f32e307abd4b166c720bd6aad57d7
오은아 <erropp89@naver.com>
2024-01-23 03:34:45 +0900

    Feat: Add Swagger Code

--- back/tingle/src/main/java/com/example/tingle/follow/controller/FollowController.java
+++ back/tingle/src/main/java/com/example/tingle/follow/controller/FollowController.java
@@ -1,6 +1,10 @@
 package com.example.tingle.follow.controller;
 
 import com.example.tingle.follow.repository.FollowRepositoryImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +14,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Tag(name = "팔로우 API")
 @RequiredArgsConstructor
 @Slf4j
 public class FollowController {
 
     private final FollowRepositoryImpl followRepository;
 
+
+    @Operation(summary = "유저가 팔로우한 스타목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "팔로우 스타 목록 조회 성공")
+    })
     @GetMapping("/follow")
     public List<String> getQuery(){
         List<String> list = followRepository.getFollows(1);
