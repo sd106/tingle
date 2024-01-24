ffb8c375270e5539807d5992784995d156a2ced5
윤정영 <dbswjddud100@naver.com>
2024-01-24 22:52:28 +0900

    Feat: Create snapshot entity/dto/repository/service

--- back/tingle/src/main/java/com/example/tingle/snapshot/entity/SnapShotEntity.java
+++ back/tingle/src/main/java/com/example/tingle/snapshot/entity/SnapShotEntity.java
@@ -1,13 +1,18 @@
 package com.example.tingle.snapshot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.tingle.user.entity.StarEntity;
+import com.example.tingle.user.entity.UserEntity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -18,7 +23,31 @@ public class SnapShotEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "snapshot_id")
     private int id;
 
-    
+    private String imageUrl;
+
+    private String content;
+
+    @OneToMany(mappedBy = "snapshot", cascade = CascadeType.ALL)
+    private Set<HashTagEntity> tags;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    private int likes;
+
+    @ManyToOne(targetEntity = UserEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(targetEntity = StarEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "star_id")
+    private StarEntity star;
+
+    public void likeChange(Integer likes) {
+        this.likes = likes;
+    }
 }
