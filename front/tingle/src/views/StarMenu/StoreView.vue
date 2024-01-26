57cd5f840c15bc73927b1bcb3b3bba39e0e36a5d
유형민 <hmy940424@naver.com>
2024-01-26 22:11:04 +0900

    Feat: 상품 등록 modal

--- front/tingle/src/views/StarMenu/StoreView.vue
+++ front/tingle/src/views/StarMenu/StoreView.vue
@@ -2,6 +2,9 @@
   <main class="container">
     <h1>스토어</h1>
     <StarMenu :name="name" />
+    <StoreMain></StoreMain>
+    <StoreCreate></StoreCreate>
+
   </main>
 </template>
 
@@ -9,6 +12,8 @@
 import { ref } from 'vue'
 
 import StarMenu from '@/components/StarMenu/StarMenu.vue';
+import StoreMain from '@/components/StarMenu/Store/StoreMain.vue';
+import StoreCreate from '@/components/StarMenu/Store/StoreCreate.vue';
 
 const props = defineProps(['username']);
 const name = ref(props.username);
