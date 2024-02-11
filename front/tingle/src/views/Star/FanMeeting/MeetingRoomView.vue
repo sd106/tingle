ee4852b34754845e443c85ce7ed74cfc5f75ca7c
윤정영 <dbswjddud100@naver.com>
2024-02-11 20:21:25 +0900

    Fix: Merge develop

--- front/tingle/src/views/Star/FanMeeting/MeetingRoomView.vue
+++ front/tingle/src/views/Star/FanMeeting/MeetingRoomView.vue
@@ -58,7 +58,7 @@ const API_URL = 'http://i10d106.p.ssafy.io:8080/'
 // WebSocket
 const initializeWebSocket = () => {
   // 소켓 초기화
-  socket = new WebSocket('ws://i10d106.p.ssafy.io:8080/signal')
+  socket = new WebSocket('ws://i10d106.p.ssafy.io:api/signal')
 
   // 소켓이 message를 받을 때 이벤트 함수
   socket.onmessage = (msg) => {
