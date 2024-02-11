d5536521597e5c52c829f1002a701659c826a745
cksghkd123 <cksghkd1234@naver.com>
2024-02-12 03:42:04 +0900

    Fix: 긴급

--- back/tingle/src/main/java/com/example/tingle/common/CorsConfig.java
+++ back/tingle/src/main/java/com/example/tingle/common/CorsConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // CorsConfig.java
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
+    @Overridegit
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true) // 브라우저가 인증 정보(예: 쿠키, HTTP 인증 관련 헤더, TLS 클라이언트 인증 등)를 함께 전송할 수 있도록 하는 역할
-                .allowedOrigins("http://localhost:5173", "http://my-tingle.us/", "https://my-tingle.us/")
+                .allowedOrigins("http://localhost:5173", "http://my-tingle.us/", "https://my-tingle.us/", "https://i10d106.p.ssafy.io/", "http://i10d106.p.ssafy.io:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
