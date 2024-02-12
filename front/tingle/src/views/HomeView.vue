33c67826b304b62390a78e98065fbd3e24263fb7
이성모 <dltjdah25@naver.com>
2024-02-12 22:43:29 +0900

    .

--- front/tingle/src/views/HomeView.vue
+++ front/tingle/src/views/HomeView.vue
@@ -12,7 +12,8 @@
     <div class="container slider-container">
       <div class="justify-content-between slider-track" ref="sliderTrack">
         <div v-for="hotstar in hotStarsInfo" :key="hotstar.id" class="p-2 star-card" style="width: 18%;">
-          <RouterLink class="router-link-custom" :to=" hotstar.id===1 ? `/${hotstar.id}/home/manage`: `/${hotstar.id}/home`">
+          <RouterLink class="router-link-custom"
+            :to="hotstar.id === 1 ? `/${hotstar.id}/home/manage` : `/${hotstar.id}/home`">
             <div class="hotstar-image">
               <img :src="hotstar.picture" alt="not" class="hotpic">
             </div>
@@ -33,12 +34,12 @@
       <button class="category-btn" @click="selectCategory(5)">기타</button>
     </div>
     <div class="container d-flex flex-wrap"
-         style="height: 360px; background-color: lightgoldenrodyellow; border-radius: 20px;">
+      style="height: 360px; background-color: lightgoldenrodyellow; border-radius: 20px;">
       <div v-if="StarsByCategory.length === 0">
         아직 이 분야 스타가 존재하지 않습니다.
       </div>
       <div v-else v-for="star in StarsByCategory" :key="star.id" class="p-2 star-card" style="width: 18%;">
-        <RouterLink class="router-link-custom" :to=" star.id===1 ? `/${star.id}/home/manage`: `/${star.id}/home`">
+        <RouterLink class="router-link-custom" :to="star.id === 1 ? `/${star.id}/home/manage` : `/${star.id}/home`">
           <div class="star-image">
             <img :src="star.picture" alt="not">
           </div>
@@ -56,7 +57,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import { useUserStore } from '@/stores/user';
 import axios from 'axios';
 
-import type { Starinfo, HotStarInfo } from '@/common/types/index'
+import type { HotStarInfo } from '@/common/types/index'
 import type { StarByCategory } from '@/common/types/index'
 
 const store = useUserStore()
@@ -85,13 +86,13 @@ const selectCategory = (category: number): void => {
   getStarsByCategory(category);
 };
 
-const hotStarsInfo= ref<HotStarInfo[]>([]);
+const hotStarsInfo = ref<HotStarInfo[]>([]);
 
 //최근 인기상승 핫스타 가져오기
 const getHotStars = async () => {
   const response = await axios.get(`${store.API_URL}/follow/hotStars`);
   hotStarsInfo.value = response.data.data;
-  if(hotStarsInfo.value.length ===0) //만약 핫스타가 없으면 구독자수가 많은 스타를 가져온다
+  if (hotStarsInfo.value.length === 0) //만약 핫스타가 없으면 구독자수가 많은 스타를 가져온다
     getTop10Stars();
   console.log(response);
 }
@@ -106,7 +107,7 @@ const getTop10Stars = async () => {
 }
 
 
-const getStarsByCategory = async (category:number) => {
+const getStarsByCategory = async (category: number) => {
   const response = await axios.get(`${store.API_URL}/star/${category}`);
   StarsByCategory.value = response.data.data;
   console.log(response);
@@ -194,13 +195,14 @@ onMounted(() => {
   overflow: hidden;
   margin-bottom: 5px;
 }
+
 .hotpic img {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
 
-.hotstar-nickName{
+.hotstar-nickName {
   width: 100%;
   height: 100%;
 }
